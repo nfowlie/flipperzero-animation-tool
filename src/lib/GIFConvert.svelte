0678b6d258233cef36a03a06691ad0aa2384253b
Noel Echo Fowlie <noel@fowlie.dev>
2023-08-20 12:44:46 -0700

    Graphics Magick Gif Converter

---
+++ src/lib/GIFConvert.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { Command } from '@tauri-apps/api/shell';
+	import { gifPath, outputPath } from '../stores';
+
+	const convertGif = async () => {
+		new Command('graphics-magick', [
+			'convert',
+			$gifPath,
+			'-resize',
+			'128x64!',
+			'-colorspace',
+			'Gray',
+			'-coalesce',
+			'+adjoin',
+			$outputPath + '/frame_%d.png'
+		]).execute();
+	};
+</script>
+
+<button on:click={convertGif}>Convert GIF to Frames</button>
+<p>${gifPath}</p>
---
+++ src/stores.js
@@ -0,0 +1,4 @@
+import { writable } from "svelte/store";
+
+export const gifPath = writable()
+export const outputPath = writable()
